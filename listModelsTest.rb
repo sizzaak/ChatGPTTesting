fc1dc04687a8206aa4d16a8446667a4223fa377b
Isaac Dowd <sizzaak@gmail.com>
2023-06-09 20:32:26 -0700

    organized model query into a class that can save and give info about models

--- listModelsTest.rb
+++ listModelsTest.rb
@@ -2,12 +2,42 @@ require 'rubygems'
 require 'dotenv'
 Dotenv.load('.env.dev')
 require 'httparty'
+require 'json'
 
-response = HTTParty.get('https://api.openai.com/v1/models', {
-  headers: {"Authorization" => "Bearer " + ENV["OPENAI_API_KEY"]}
-})
+class OpenAIInterface
+  def initialize()
+    @models = []
+  end
 
-puts response.message
-puts
-puts
-puts response.body
+  # Requests a list of models from OpenAI and saves the list if successful,
+  # returning the response code of the curl request
+  def queryModels()
+    response = HTTParty.get('https://api.openai.com/v1/models', {
+      headers: {"Authorization" => "Bearer " + ENV["OPENAI_API_KEY"]}
+    })
+    if response.code == 200
+      parsed = JSON.parse(response.body)
+      @models = parsed["data"]
+    end
+    return response.code
+  end
+
+  # Returns the list of models as an array of hashes containing all model info
+  def getModels()
+    return @models
+  end
+
+  # Returns a list of just the names of all OpenAI models
+  def getModelNames()
+    list = []
+    for m in @models
+      list.push(m["id"])
+    end
+    return list
+  end
+
+end
+
+interface = OpenAIInterface.new
+puts interface.queryModels
+puts interface.getModelNames
